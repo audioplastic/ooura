6b9992c81aca8621c38ced9e3a5f24669c2681ce
Nick Clark <audioplastic@users.noreply.github.com>
2017-08-30 18:37:27 +0100

    Fully tested for correctness

--- spec/setup-spec.js
+++ spec/setup-spec.js
@@ -2,24 +2,20 @@ var Ooura = require('../ooura.js')
 
 describe("During object setup", function () {
 
-    it("should create a valid object with correct size", function () {
+    it("should create a valid object with correct real-size and complex-size", function () {
         const testSize = 32
         let oo = new Ooura(testSize);
         expect(oo.size).toBe(testSize);
-        console.log("ip " + oo.ip);
-        console.log("w " + oo.w);
+        expect(oo.getComplexSize()).toBe(1+ testSize/2);
+    });
 
-        let data = new Float64Array([1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]);
-        let dataOut = new Float64Array(testSize);
-        let re = new Float64Array(oo.getComplexSize());
-        let im = new Float64Array(Ooura.complexSize(testSize));
-        oo.fft(data.buffer, re.buffer, im.buffer);
-        console.log("buffer " + oo.internal);
-        console.log("re " + re);
-        console.log("im " + im);
-        oo.ifft(dataOut.buffer, re.buffer, im.buffer);
-        console.log("buffer " + oo.internal);
-        console.log("data out " + dataOut);
+    it("should have a functional complex size static member helper", function () {
+        expect(Ooura.complexSize(32)).toBe(17);
+        expect(Ooura.complexSize(128)).toBe(65);
+    });
 
+    it("should have a functional real size static member helper", function () {
+        expect(Ooura.realSize(17)).toBe(32);
+        expect(Ooura.realSize(65)).toBe(128);
     });
 });
