27f5dbc6c79396dc78b49b034717e50d3ab333e4
Nick Clark <audioplastic@users.noreply.github.com>
2017-08-30 21:20:02 +0100

    Agh! Type on the .circleci folder name ... and I was blaming the node version

--- spec/run.js
+++
@@ -1,8 +0,0 @@
-// From here https://gist.github.com/mauvm/172878a9646095d03fd7
-// While Circle CI 2.0 still has crusty old node images
-
-import Jasmine from 'jasmine'
-
-var jasmine = new Jasmine()
-jasmine.loadConfigFile('spec/support/jasmine.json')
-jasmine.execute()
